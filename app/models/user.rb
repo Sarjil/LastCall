da654cb79dcda1ee87d7240eb905b4fbc7580519
sarjilmiah <sarjilmiah@gmail.com>
2020-04-06 18:11:52 -0400

    fixing heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,16 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-    validates: :username, presence: true, uniqueness: true
-    validates: :email, presence: true, uniqueness: true
-    validates: :password_digest, presence: true
-    validates: :password, length:{minimum:6, allow_nil: true}
+
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length:{minimum: 6, allow_nil: true}
+    after_initialize :ensure_session_token
 
     attr_reader :password
+    #FIGVAPER 
 
-     def self.find_by_credentials(username,password)
+    def self.find_by_credentials(username,password)
         @user = User.find_by(username: username)
         return nil unless @user && @user.is_password?(password)
         return @user
@@ -46,5 +48,6 @@ class User < ApplicationRecord
         self.save
         self.session_token
     end 
-    
+
+
 end
