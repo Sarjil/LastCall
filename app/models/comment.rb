d93ba7cae34f645373493353c98587eb172ce40e
sarjilmiah <sarjilmiah@gmail.com>
2020-04-15 14:24:12 -0400

    reseeding db

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,6 +12,6 @@ class Comment < ApplicationRecord
     validates :body, :author_id, presence: true 
 
     belongs_to :author,
-    foreign_key: :author_id
+    foreign_key: :author_id,
     class_name: :User 
 end
